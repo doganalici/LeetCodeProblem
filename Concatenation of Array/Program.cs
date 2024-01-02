434bb0f496d94b1f473315d8055307510b577b4c
Dogan Alici <doganalici2144@gmail.com>
2024-01-02 21:35:03 +0300

    Add project files.

---
+++ Concatenation of Array/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Concatenation_of_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 2, 1 };
+            int[] ans = new int[nums.Length * 2];
+
+            var result = GetConcatenation(nums);
+            int[] GetConcatenation(int[] nums)
+            {
+                int index = 0;
+                for (int i = 0; i <= 1; i++)
+                {
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        ans[index] = nums[j];
+                        index++;
+                    }
+                }
+                var d = ans;
+                return ans;
+            }
+
+
+        }
+    }
+}
