58ba853fa4cec4829a28f0a9ccbf090aea590332
Dogan Alici <doganalici2144@gmail.com>
2024-01-09 08:14:52 +0300

    yeni

--- Find the Maximum Achievable Number/Program.cs
+++ Find the Maximum Achievable Number/Program.cs
@@ -1,6 +1,6 @@
 ﻿//int num = 4, t = 1;
 int num = 3, t = 2;
-int result = TheMaximumAchievableX(num,t);
+int result = TheMaximumAchievableX(num, t);
 var d = result;
 
 int TheMaximumAchievableX(int num, int t)
